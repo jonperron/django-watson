76f349b751c02dd57175f7f8fb8492742d0ef1ec
Jonathan Perron <jonperron@users.noreply.github.com>
2017-09-28 11:07:21 +0200

    Update installwatson.py

--- watson/management/commands/installwatson.py
+++ watson/management/commands/installwatson.py
@@ -3,6 +3,7 @@
 from __future__ import unicode_literals
 
 from django.core.management.base import BaseCommand
+from django.db import connection
 
 from watson.search import get_backend
 
@@ -15,10 +16,11 @@ class Command(BaseCommand):
         """Runs the management command."""
         verbosity = int(options.get("verbosity", 1))
         backend = get_backend()
+        schema_name = connection.tenant.schema_name
         if not backend.requires_installation:
             if verbosity >= 2:
                 self.stdout.write("Your search backend does not require installation.\n")
-        elif backend.is_installed():
+        elif backend.is_installed(schema_name):
             if verbosity >= 2:
                 self.stdout.write("django-watson is already installed.\n")
         else:
